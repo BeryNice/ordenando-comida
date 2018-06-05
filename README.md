16d3ec17c438684c7ec0873dd2e9ce18c0478875
BeryNice <berenice.lemus@gmail.com>
2018-06-05 05:48:41 -0500

    agreando index html, app.js y readme.md

---
+++ README.md
@@ -0,0 +1,1 @@
+1 ordenando comida del menu
---
+++ app.js
@@ -0,0 +1,23 @@
+
+//ceviche
+var ceviche;
+// tacos al pastor
+var tacosAlPAstor;
+// empanada chilena
+var empanadaChilena;
+//Hamburguesa
+var hamburguesa;
+
+
+var menu = function (miMenu){
+  if (ceviche = false) {
+    alert("Quiero pedir ceviche");
+  } else if (tacosAlPAstor = true) {
+    alert("Quiero ordenar Tacos al Pastor");
+  } else if (empanadaChilena) {
+    alert("Quiero una Empanada Chilena");
+  }else {
+    alert("Quiero una Hamburguesa");
+  }
+}
+menu();
